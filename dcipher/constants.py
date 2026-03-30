e4b757c6ea5e39d334bdb5523e2b82f78e6a0fb1
Zafer Cavdar <zafercavdar@yahoo.com>
2026-03-30 15:55:21 +0200

    Retry after json decode errors

--- dcipher/constants.py
+++ dcipher/constants.py
@@ -1,8 +1,12 @@
+import json
+
 from httpx import NetworkError, TimeoutException
 from requests.exceptions import ConnectionError, Timeout
 
 from .exceptions import ConflictError, InternalServerError, RateLimitError
 
+# json.JSONDecodeError covers response.json() failures (requests + httpx).
 RETRIABLE_EXCEPTIONS = (ConnectionError, Timeout,
                         ConflictError, RateLimitError,
-                        InternalServerError, TimeoutException, NetworkError)
+                        InternalServerError, TimeoutException, NetworkError,
+                        json.JSONDecodeError)
